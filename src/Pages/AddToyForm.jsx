f01a02ea94e59f2b43aa43415f18b9200b9b42ce
hasib1511 <hasib7143@gmail.com>
2023-05-18 04:59:09 +0600

    added Toy Form

---
+++ src/Pages/AddToyForm.jsx
@@ -0,0 +1,216 @@
+import React, { useState } from "react";
+
+const AddToyForm = () => {
+  const [formValues, setFormValues] = useState({
+    pictureUrl: "",
+    name: "",
+    sellerName: "",
+    sellerEmail: "",
+    subCategory: "",
+    price: "",
+    rating: "",
+    quantity: "",
+    description: "",
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormValues({ ...formValues, [name]: value });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const pictureUrl = form.pictureUrl.value;
+    const name = form.name.value;
+    const sellerName = form.sellerName.value;
+    const sellerEmail = form.sellerEmail.value;
+    const subCategory = form.subCategory.value;
+    const price = form.price.value;
+    const rating = form.rating.value;
+    const quantity = form.quantity.value;
+    const description = form.description.value;
+
+    // Perform form submission or data handling here
+    console.log("Form submitted:");
+    console.log("Picture URL:", pictureUrl);
+    console.log("Name:", name);
+    console.log("Seller Name:", sellerName);
+    console.log("Seller Email:", sellerEmail);
+    console.log("Sub-category:", subCategory);
+    console.log("Price:", price);
+    console.log("Rating:", rating);
+    console.log("Quantity:", quantity);
+    console.log("Description:", description);
+
+    // Reset the form fields
+    form.reset();
+  };
+
+  return (
+    <div className="container mx-auto w-6/12 mt-">
+      <h1 className="text-2xl font-bold mb-4">Add A Toy</h1>
+      <form onSubmit={handleSubmit}>
+        <div className="mb-4">
+          <label
+            htmlFor="pictureUrl"
+            className="block text-gray-700 font-medium mb-2"
+          >
+            Picture URL of the toy
+          </label>
+          <input
+            type="text"
+            id="pictureUrl"
+            name="pictureUrl"
+            value={formValues.pictureUrl}
+            onChange={handleInputChange}
+            className="border border-gray-300 rounded-md px-3 py-2 w-full focus:outline-none focus:ring focus:ring-blue-500"
+            required
+          />
+        </div>
+        <div className="mb-4">
+          <label
+            htmlFor="name"
+            className="block text-gray-700 font-medium mb-2"
+          >
+            Name
+          </label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            value={formValues.name}
+            onChange={handleInputChange}
+            className="border border-gray-300 rounded-md px-3 py-2 w-full focus:outline-none focus:ring focus:ring-blue-500"
+            required
+          />
+        </div>
+        <div className="mb-4">
+          <label
+            htmlFor="sellerName"
+            className="block text-gray-700 font-medium mb-2"
+          >
+            Seller Name
+          </label>
+          <input
+            type="text"
+            id="sellerName"
+            name="sellerName"
+            value={formValues.sellerName}
+            onChange={handleInputChange}
+            className="border border-gray-300 rounded-md px-3 py-2 w-full focus:outline-none focus:ring focus:ring-blue-500"
+          />
+        </div>
+        <div className="mb-4">
+          <label
+            htmlFor="sellerEmail"
+            className="block text-gray-700 font-medium mb-2"
+          >
+            Seller Email
+          </label>
+          <input
+            type="email"
+            id="sellerEmail"
+            name="sellerEmail"
+            value={formValues.sellerEmail}
+            onChange={handleInputChange}
+            className="border border-gray-300 rounded-md px-3 py-2 w-full focus:outline-none focus:ring focus:ring-blue-500"
+          />
+        </div>
+        <div className="mb-4">
+          <label
+            htmlFor="subCategory"
+            className="block text-gray-700 font-medium mb-2"
+          >
+            Sub-category
+          </label>
+          <input
+            type="text"
+            id="subCategory"
+            name="subCategory"
+            value={formValues.subCategory}
+            onChange={handleInputChange}
+            className="border border-gray-300 rounded-md px-3 py-2 w-full focus:outline-none focus:ring focus:ring-blue-500"
+            required
+          />
+        </div>
+        <div className="mb-4">
+          <label
+            htmlFor="price"
+            className="block text-gray-700 font-medium mb-2"
+          >
+            Price
+          </label>
+          <input
+            type="number"
+            id="price"
+            name="price"
+            value={formValues.price}
+            onChange={handleInputChange}
+            className="border border-gray-300 rounded-md px-3 py-2 w-full focus:outline-none focus:ring focus:ring-blue-500"
+            required
+          />
+        </div>
+        <div className="mb-4">
+          <label
+            htmlFor="rating"
+            className="block text-gray-700 font-medium mb-2"
+          >
+            Rating
+          </label>
+          <input
+            type="number"
+            id="rating"
+            name="rating"
+            value={formValues.rating}
+            onChange={handleInputChange}
+            className="border border-gray-300 rounded-md px-3 py-2 w-full focus:outline-none focus:ring focus:ring-blue-500"
+            required
+          />
+        </div>
+        <div className="mb-4">
+          <label
+            htmlFor="quantity"
+            className="block text-gray-700 font-medium mb-2"
+          >
+            Available Quantity
+          </label>
+          <input
+            type="number"
+            id="quantity"
+            name="quantity"
+            value={formValues.quantity}
+            onChange={handleInputChange}
+            className="border border-gray-300 rounded-md px-3 py-2 w-full focus:outline-none focus:ring focus:ring-blue-500"
+            required
+          />
+        </div>
+        <div className="mb-4">
+          <label
+            htmlFor="description"
+            className="block text-gray-700 font-medium mb-2"
+          >
+            Detail Description
+          </label>
+          <textarea
+            id="description"
+            name="description"
+            value={formValues.description}
+            onChange={handleInputChange}
+            className="border border-gray-300 rounded-md px-3 py-2 w-full focus:outline-none focus:ring focus:ring-blue-500"
+            rows={4}
+            required
+          ></textarea>
+        </div>
+        <button
+          type="submit"
+          className="bg-blue-500 hover:bg-blue-600 text-white font-medium py-2 px-4 rounded-md"
+        >
+          Add Toy
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default AddToyForm;
